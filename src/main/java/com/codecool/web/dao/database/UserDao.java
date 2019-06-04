d5c521a9e011ec40fb26578705feb45878593e6f
barnadenes <nyukk1007@gmail.com>
2019-06-04 11:56:27 +0200

    UserDao update addnewusr, getallusr

--- src/main/java/com/codecool/web/dao/database/UserDao.java
+++ src/main/java/com/codecool/web/dao/database/UserDao.java
@@ -3,9 +3,11 @@ package com.codecool.web.dao.database;
 import com.codecool.web.model.User;
 
 import java.sql.SQLException;
+import java.util.List;
 
 public interface UserDao {
 
     User findByEmail(String email) throws SQLException;
-
+    User addNewUser(String name, String email, String password) throws SQLException;
+    List<User> getUserList() throws SQLException;
 }
