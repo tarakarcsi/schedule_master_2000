067c53721aa17d8add4d51e6d457f5992aa50878
beszteri <lajka89@gmail.com>
2019-06-04 11:40:50 +0200

    Login Servlet

---
+++ src/main/java/com/codecool/web/servlet/LoginServlet.java
@@ -0,0 +1,43 @@
+package com.codecool.web.servlet;
+
+import com.codecool.web.dao.database.DatabaseUserDao;
+import com.codecool.web.dao.database.UserDao;
+import com.codecool.web.model.User;
+import com.codecool.web.service.LoginService;
+import com.codecool.web.service.Simple.SimpleLoginService;
+import com.codecool.web.service.exception.ServiceException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.sql.rowset.serial.SerialException;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+@WebServlet("/login")
+public class LoginServlet extends AbstractServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        try (Connection connection = getConnection(req.getServletContext())) {
+            UserDao userDao = new DatabaseUserDao(connection);
+            LoginService loginService = new SimpleLoginService(userDao);
+
+            String email = req.getParameter("email");
+            String password = req.getParameter("password");
+
+            User user = loginService.loginUser(email, password);
+            req.getSession().setAttribute("user", user);
+
+            sendMessage(resp, HttpServletResponse.SC_OK, user);
+        }catch (SerialException e){
+            sendMessage(resp, HttpServletResponse.SC_UNAUTHORIZED, e.getMessage());
+        }catch (SQLException e ){
+            handleSqlError(resp, e);
+        }catch (ServiceException e){
+            e.printStackTrace();
+        }
+    }
+}
