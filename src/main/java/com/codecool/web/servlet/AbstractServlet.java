b6989a76a93b5623b5b04bedd516d3332017dbfe
beszteri <lajka89@gmail.com>
2019-06-04 11:48:52 +0200

    error fixing in abstractservlet

--- src/main/java/com/codecool/web/servlet/AbstractServlet.java
+++ src/main/java/com/codecool/web/servlet/AbstractServlet.java
@@ -1,6 +1,7 @@
 package com.codecool.web.servlet;
 
 //import com.codecool.web.dto.MessageDto;
+import com.codecool.web.dto.MessageDto;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import javax.servlet.ServletContext;
@@ -20,9 +21,9 @@ abstract class AbstractServlet extends HttpServlet {
         return dataSource.getConnection();
     }
 
-//    void sendMessage(HttpServletResponse resp, int status, String message) throws IOException {
-//        sendMessage(resp, status, new MessageDto(message));
-//    }
+    void sendMessage(HttpServletResponse resp, int status, String message) throws IOException {
+      sendMessage(resp, status, new MessageDto(message));
+    }
 
     void sendMessage(HttpServletResponse resp, int status, Object object) throws IOException {
         resp.setStatus(status);
