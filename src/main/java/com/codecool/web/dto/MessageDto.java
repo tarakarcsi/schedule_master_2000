dc1cb8e9479b9ad0cc782d13959c4c5c513124be
beszteri <lajka89@gmail.com>
2019-06-04 11:46:15 +0200

    MessageDto

---
+++ src/main/java/com/codecool/web/dto/MessageDto.java
@@ -0,0 +1,15 @@
+package com.codecool.web.dto;
+
+public class MessageDto {
+
+    private final String message;
+
+    public MessageDto(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+}
--- src/main/java/com/codecool/web/dto/UserDto.java
+++
@@ -1,4 +0,0 @@
-package com.codecool.web.dto;
-
-public class UserDto {
-}
