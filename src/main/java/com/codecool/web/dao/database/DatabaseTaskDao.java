b0fa4e9b16fb3560c1688d5ebb492b7c16b0d92e
barnadenes <nyukk1007@gmail.com>
2019-06-05 11:44:29 +0200

    task dao update

--- src/main/java/com/codecool/web/dao/database/DatabaseTaskDao.java
+++ src/main/java/com/codecool/web/dao/database/DatabaseTaskDao.java
@@ -1,8 +1,10 @@
 package com.codecool.web.dao.database;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import com.codecool.web.model.Task;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class DatabaseTaskDao extends AbstractDao implements TaskDao{
 
@@ -20,5 +22,35 @@ public class DatabaseTaskDao extends AbstractDao implements TaskDao{
         }catch (SQLException e){
             e.printStackTrace();
         }
+    }
+
+    @Override
+    public List<Task> getTaskList() throws SQLException {
+        String sql = "SELECT * FROM tasks;";
+        List<Task> tasks = new ArrayList<>();
+        try  (Statement statement = connection.createStatement();
+              ResultSet resultSet = statement.executeQuery(sql)) {
+            while (resultSet.next()){
+                tasks.add(getTask(resultSet));
+            }
+        }
+        return tasks;
+    }
+
+    @Override
+    public void removeTaskByTitle(String title) throws SQLException {
+        // To be implemented.
+    }
+
+    @Override
+    public void updateTask(Task task) throws SQLException {
+        // To be implemented.
+    }
+
+    private Task getTask(ResultSet resultSet) throws SQLException {
+        int id = resultSet.getInt("taskId");
+        String title = resultSet.getString("title");
+        String content = resultSet.getString("content");
+        return new Task(id, title, content);
     }
 }
--- src/main/java/com/codecool/web/dao/database/TaskDao.java
+++ src/main/java/com/codecool/web/dao/database/TaskDao.java
@@ -8,4 +8,6 @@ import java.util.List;
 public interface TaskDao {
     void addNewTask(String title, String content) throws SQLException;
     List<Task> getTaskList() throws SQLException;
+    void removeTaskByTitle(String title) throws  SQLException;
+    void updateTask(Task task) throws SQLException;
 }
