bfb262e0c8747eadcd137fbe69e5e6144fa05890
barnadenes <nyukk1007@gmail.com>
2019-06-17 15:04:34 +0200

    Simple Schedule

--- src/main/java/com/codecool/web/service/ScheduleService.java
+++ src/main/java/com/codecool/web/service/ScheduleService.java
@@ -1,6 +1,13 @@
 package com.codecool.web.service;
+import com.codecool.web.model.Schedule;
+import com.codecool.web.service.exception.ServiceException;
+
+import java.sql.SQLException;
+import java.util.List;
 
 public interface ScheduleService {
-
+    Schedule findAllSchedules(String email) throws SQLException, ServiceException;
+    Schedule findScheduleById(int scheduleId)throws SQLException, IllegalArgumentException, ServiceException;
+    List<Schedule> findAllSchedulesByUserId(int userId) throws SQLException, ServiceException;
 
 }
--- src/main/java/com/codecool/web/service/Simple/SimpleScheduleService.java
+++ src/main/java/com/codecool/web/service/Simple/SimpleScheduleService.java
@@ -1,8 +1,32 @@
 package com.codecool.web.service.Simple;
-
+import com.codecool.web.dao.database.ScheduleDao;
+import com.codecool.web.model.Schedule;
 import com.codecool.web.service.ScheduleService;
+import com.codecool.web.service.exception.ServiceException;
+
+import java.sql.SQLException;
+import java.util.List;
 
 public class SimpleScheduleService implements ScheduleService {
+    private final ScheduleDao scheduleDao;
 
-    
+    public SimpleScheduleService(ScheduleDao scheduleDao) {
+        this.scheduleDao = scheduleDao;
+    }
+
+
+    @Override
+    public Schedule findAllSchedules(String email) throws SQLException, ServiceException {
+        
+    }
+
+    @Override
+    public Schedule findScheduleById(int scheduleId) throws SQLException, IllegalArgumentException, ServiceException {
+        return null;
+    }
+
+    @Override
+    public List<Schedule> findAllSchedulesByUserId(int userId) throws SQLException, ServiceException {
+        return null;
+    }
 }
